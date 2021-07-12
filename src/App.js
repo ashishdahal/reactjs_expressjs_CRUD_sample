30ee05c799811a1bfd3e4291128861736f6986b3
Ash Dahal <ashdahal@ash-book.attlocal.net>
2021-07-12 07:37:41 -0500

    Initialized webpage using react with a card displaying name,age and task details with a button to update card info.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Card from './Components/Card'
+import React, { useState } from 'react'
 
 function App() {
-  return (
+const [yname,setYname] = useState("Displays Your Name");
+const [age,setAge] = useState("Displays Your Age");
+const [title,setTitle] = useState("Displays title of The task");
+const [date,setDate] = useState("Displays When it needs to be finished");
+const [task,setTask] = useState("Displays description of the task")
+
+
+
+function collectUserInfo() {
+ 
+  let na = prompt("What is your Name?");
+  setYname("Name: "+na);
+  let ag = prompt("What is your Age?");
+  setAge("Age: "+ag);
+  let ti = prompt("What is the Title of the Task");
+  setTitle("Task Title: "+ti);
+  let da = prompt("What is the Date it needs to be Finished?");
+  setDate("Date to Complete: "+da);
+  let ta = prompt("Describe the task please!");
+  setTask("Task Description: "+ta);
+
+}
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button onClick={collectUserInfo}  className="but" >Click me to Update Card</button>
+      <Card yname={yname} age={age} title={title} date={date} task={task} />
+    </div>  
   );
+
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import Card from './Components/Card'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <App/>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
