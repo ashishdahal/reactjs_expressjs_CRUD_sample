1f7eeb85816e84d21976ed7a760c3124dfc63206
Ash Dahal <ashdahal@ash-book.attlocal.net>
2021-07-13 00:29:22 -0500

    Removed button and implemented dynamic change of values displayed on card using onChange react function

--- src/App.js
+++ src/App.js
@@ -11,34 +11,18 @@ const [title,setTitle] = useState("Displays title of The task");
 const [date,setDate] = useState("Displays When it needs to be finished");
 const [task,setTask] = useState("Displays description of the task")
 
-function collectUserInfo() {
- 
-
-  setYname("Name: "+document.getElementsByTagName("input")[0].value);
-
-  setAge("Age: "+document.getElementsByTagName("input")[1].value);
-
-  setTitle("Task Title: "+document.getElementsByTagName("input")[2].value);
-
-  setDate("Date to Complete: "+document.getElementsByTagName("input")[3].value);
-
-  setTask("Task Description: "+document.getElementsByTagName("input")[4].value);
-
-}
-
 return (
     <div className="App">
       
       <div className = "inputContainer">
-      <input className = "name" placeholder="Enter your name:"></input>
-      <input className = "age" placeholder="Enter your age:"></input>
-      <input className = "title" placeholder="Enter Task Title:"></input>
-      <input className = "date" placeholder="Enter due date of task:"></input>
-      <input className = "task" placeholder="Describe your task"></input>
-      <button onClick={collectUserInfo}  className="but" >Click to Update Card</button>
+      <h1>Enter the values you need to update.</h1>
+      <input className = "name" placeholder="Enter your name:" onChange={e => setYname(e.target.value)} ></input>
+      <input className = "age" placeholder="Enter your age:" onChange={e => setAge(e.target.value)}></input>
+      <input className = "title" placeholder="Enter Task Title:" onChange={e => setTitle(e.target.value)}></input>
+      <input className = "date" placeholder="Enter due date of task:" onChange={e => setDate(e.target.value)}></input>
+      <input className = "task" placeholder="Describe your task" onChange={e => setTask(e.target.value)}></input>
 
     </div>
-      
       <Card yname={yname} age={age} title={title} date={date} task={task} />
     </div>  
   );
